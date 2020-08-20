639f326b48992d53f8196c1ec91ce621ec0898ef
ahmaag951 <thisismyway_2011@yahoo.com>
2020-08-20 12:28:42 +0200

    add rest sharp general

---
+++ 23.test_rest_sharp_general/23.test_rest_sharp/test_rest_sharp/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace test_rest_sharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            // we need to call https://localhost:44313/api/values
+            var client = new RestClient("https://localhost:44313/api/");
+
+            var request = new RestRequest("values");
+            //request.AddParameter("string name", "object value");
+
+            List<string> test = new List<string>();
+
+            var response1 = client.Get(request);
+            var response2 = client.ExecuteAsync<List<string>>(request,
+                (response) => test = response.Data);
+
+            TestAwait(client, request);
+
+
+            return test;
+        }
+
+        private void TestAwait(RestClient client, RestRequest request)
+        {
+            client.ExecuteAsync(request, CallBack);
+        }
+
+        private void CallBack(IRestResponse arg1, RestRequestAsyncHandle arg2)
+        {
+            var debug = arg1.Content;
+        }
+    }
+}
---
+++ 23.test_rest_sharp_general/23.test_rest_sharp/to_be_called/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace to_be_called.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
